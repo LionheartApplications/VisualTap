d85f5ef2fdba8c5d27edce6a8af3d8a03eb1127f
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2021-03-29 11:01:10 +0300

    workspace setup complete

---
+++ Example/VisualTapExample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  VisualTapExample
+//
+//  Created by Stoyan Stoyanov on 29/03/21.
+//
+
+import UIKit
+
+
+// MARK: - Class Definition
+
+final class ViewController: UIViewController {
+
+}
+
+// MARK: - User Actions
+
+extension ViewController {
+    
+    @IBAction private func startTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func stopTapped(_ sender: UIButton) {
+        
+    }
+}
---
+++ Source/VisualTap/VisualTap.h
@@ -0,0 +1,18 @@
+//
+//  VisualTap.h
+//  VisualTap
+//
+//  Created by Stoyan Stoyanov on 29/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisualTap.
+FOUNDATION_EXPORT double VisualTapVersionNumber;
+
+//! Project version string for VisualTap.
+FOUNDATION_EXPORT const unsigned char VisualTapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisualTap/PublicHeader.h>
+
+
